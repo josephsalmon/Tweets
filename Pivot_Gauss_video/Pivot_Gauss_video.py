ac6e8749348983fb45c126b89a40e1bdbb8e4c3f
josephsalmon <joseph.salmon.taff@gmail.com>
2019-12-17 22:50:03 +0100

    end for full gauss pivot

--- Pivot_Gauss_video/Pivot_Gauss_video.py
+++ Pivot_Gauss_video/Pivot_Gauss_video.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import os
 
 save = True
+np.random.seed(44)
 
 if not os.path.exists("./gifs"):
     os.mkdir("./gifs")
@@ -45,7 +46,10 @@ def gaussian_elimination_with_pivot(m):
     n, _ = m.shape
     liste_evol = []
     liste_mat = []
+    liste_mat_elem = []
+
     m_evol = np.eye(n)
+
     for i in range(n):
         # ACTIVATE IF you want pivot.
         # pivot(m, m_evol, i)
@@ -55,10 +59,13 @@ def gaussian_elimination_with_pivot(m):
             alpha = m[j, i] / m[i, i]
             m[j, :] = m[j, :] - m[i, :] * alpha
             m_evol[j, :] = m_evol[j, :] - m_evol[i, :] * alpha
+            m_elem = np.eye(n)
+            m_elem[j, i] = - alpha
             liste_mat.append(m.copy())
             liste_evol.append(m_evol.copy())
+            liste_mat_elem.append(m_elem.copy())
         liste_evol.append(m_evol.copy())
-
+        liste_mat_elem.append(m_elem.copy())
     # if np.abs(m[n - 1, n - 1]) < np.finfo(np.float64).eps:
     #     raise ValueError('No unique solution')
 
@@ -67,15 +74,17 @@ def gaussian_elimination_with_pivot(m):
             alpha = m[j, i] / m[i, i]
             m[j, :] = m[j, :] - m[i, :] * alpha
             m_evol[j, :] = m_evol[j, :] - m_evol[i, :] * alpha
+            m_elem = np.eye(n)
+            m_elem[j, i] = - alpha
             liste_evol.append(m_evol.copy())
             liste_mat.append(m.copy())
-
+            liste_mat_elem.append(m_elem.copy())
     # # backward substitution
     # x = np.zeros(n, )
     # for i in range(n - 1, -1, -1):
     #     s = sum(m[i,j] * x[j] for j in range(i, n))
     #     x[i] = (m[i,n] - s) / m[i,i]
-    return liste_mat, liste_evol
+    return liste_mat, liste_evol, liste_mat_elem
 
 '''
 # shorter way to pivot but cannot run in trinket
@@ -87,13 +96,18 @@ def pivot(m, n, i):
 
 def imaging(m):
     m_init = m.copy()
-    liste_mat, liste_evol = gaussian_elimination_with_pivot(m)
+    liste_mat, liste_evol, liste_mat_elem = gaussian_elimination_with_pivot(m)
     fig = plt.figure(figsize=(4, 4))
+    ax11 = fig.add_subplot(2, 2, 1)
     ax12 = fig.add_subplot(2, 2, 2)
     ax21 = fig.add_subplot(2, 2, 3)
     ax22 = fig.add_subplot(2, 2, 4)
     plt.show(block=False)
     for i, matrix in enumerate(liste_mat):
+
+        ax11.set_axis_off()
+        ax11.imshow(liste_mat_elem[i], vmin=-3, vmax=3, cmap='RdBu')
+
         ax21.set_axis_off()
         ax21.imshow(liste_evol[i], vmin=-3, vmax=3, cmap='RdBu')
 
